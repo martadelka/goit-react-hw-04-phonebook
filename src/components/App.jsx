2c6dbe63d768827d4bf0326ff984e89bf2164d9b
martadelka <oamedvedeva777@gmail.com>
2023-11-28 22:58:11 +0200

    task 2.0

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -72,16 +72,3 @@ export const App = () => {
   </section>
   );
 }
-
-// return (
-//     <div className={css.container}>
-//       <h1>Phonebook</h1>
-//       <ContactForm onSubmit={addContact} />
-//       <h2>Contacts</h2>
-//       <Filter value={filter} onChange={changeFilter} />
-//       <ContactList
-//         deleteContact={deleteContact}
-//         contacts={getVisibleContacts()}
-//       />
-//     </div>
-//   );
